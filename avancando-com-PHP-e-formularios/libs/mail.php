158742d6abb39abbc8cf596d1262935e060980c4
Danilo Alexandrino <danilo.alexandrinodm@gmail.com>
2020-02-25 20:51:20 -0300

    Segurança de formulário e envio de email

---
+++ avancando-com-PHP-e-formularios/libs/mail.php
@@ -0,0 +1,21 @@
+<?php
+
+$to = 'danilo.alexandrinodm@gmail.com';
+$subject = 'Email de teste com PHP';
+$message = "
+<p><strong>Nome</strong>: {$nome}</p>
+<p><strong>URL</strong>: {$url}</p>
+<p><strong>Email</strong>: {$email}</p>
+<p><strong>Mensagem</strong>: {$descricao}</p>
+";
+
+$additional_headers = 'From: ' . $email . '\r\n';
+$additional_headers .= 'Reply-To: ' . $email . '\r\n';
+$additional_headers .= 'X-Mailer: ' . phpversion();
+
+// se utiliza linux, instalar postfix
+if (mail($to, $subject, $message, $additional_headers)) {
+    die('Ok, email enviado');
+} else {
+    die('Falha ao enviar');
+}
