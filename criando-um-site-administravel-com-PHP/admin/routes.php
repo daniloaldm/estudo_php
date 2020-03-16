eaca7b3d27205c2a91dc9d19c2ef2d831ca848fa
Danilo Alexandrino <danilo.alexandrinodm@gmail.com>
2020-03-15 21:33:12 -0300

    Retornando status code correto

--- criando-um-site-administravel-com-PHP/admin/routes.php
+++ criando-um-site-administravel-com-PHP/admin/routes.php
@@ -7,5 +7,8 @@ if (resolve('/admin') || resolve('/admin/')){
 }elseif(resolve('/admin/pages') || resolve('/admin/pages/')){
     render('admin/pages', 'admin');
 }else{
+    http_response_code(404); 
+    $path = $_SERVER['PATH_INFO'] ?? '/';
+    var_dump($path);
     echo 'Página não encontrada';
 }
--- criando-um-site-administravel-com-PHP/site/routes.php
+++ criando-um-site-administravel-com-PHP/site/routes.php
@@ -5,6 +5,7 @@ if (resolve('/') || resolve('/site/') || resolve('/site')){
 }elseif(resolve('/contato') || resolve('/contato/') || resolve('/site/contato') || resolve('/site/contato/')){
     render ('site/contato', 'site');
 }else{
+    http_response_code(404);
     $path = $_SERVER['PATH_INFO'] ?? '/';
     var_dump($path);
     echo 'Página não encontrada';
