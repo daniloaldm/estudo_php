9ef1c2285148236a4ece22b0640d5c426b5de56f
Danilo Alexandrino <danilo.alexandrinodm@gmail.com>
2020-03-15 22:58:41 -0300

    Estilizando o template do painel de administração

--- criando-um-site-administravel-com-PHP/src/connection.php
+++ criando-um-site-administravel-com-PHP/src/connection.php
@@ -4,7 +4,7 @@ $conn = new mysqli(DB_SERVER, DB_USER, DB_PASSWD, DB_NAME);
 if($conn->connect_errno){
     die('Falhou em conectar: (' . $conn->connect_errno . ') ' . $conn->connect_error);
 }else{
-    echo "<h6>Conectado!!</h6>";
+    // echo "<h6>Conectado!!</h6>";
 }
 
 return $conn;
--- criando-um-site-administravel-com-PHP/templates/admin.tpl.php
+++ criando-um-site-administravel-com-PHP/templates/admin.tpl.php
@@ -1,22 +1,57 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href=<?= basename(__DIR__) . "/../../../public/css/bootstrap.min.css"?> integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    <link rel="stylesheet" href=<?= basename(__DIR__) . "/../../../public/css/bootstrap.min.css" ?> integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href=<?= basename(__DIR__) . "/../../../public/css/style.css" ?>>
     <title>Painel Administrativo</title>
-  </head>
-  <body>
-    <?php include $content?>
+</head>
+
+<body class="d-flex flex-column">
+    <div id="header">
+        <nav class="navbar navbar-dark bg-dark">
+            <span>
+                <a href="" class="navbar-brand">Admin</a>
+                <span class="navbar-text">
+                    Painel Administrativo
+                </span>
+            </span>
+            <a href="" class="btn btn-danger"><i class="fas fa-sign-out-alt"></i></a>
+        </nav>
+    </div>
+
+    <div id="main">
+        <div class="row">
+            <div class="col">
+                <ul id="main-menu" class="nav flex-column nav-pills bg-secondary text-white p-2">
+                    <li class="nav-item">
+                       <span href="" class="nav-link text-white-50"><small>MENU</small></span>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">Páginas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Usuários</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="content" class="col-10">
+                <?php include $content ?>
+            </div>
+        </div>
+
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src=<?= basename(__DIR__) . "/../../../public/js/bootstrap.min.js"?> integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
+    <script src=<?= basename(__DIR__) . "/../../../public/js/bootstrap.min.js" ?> integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
 </html>
